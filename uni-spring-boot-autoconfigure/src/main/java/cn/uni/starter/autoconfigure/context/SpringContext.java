5375a67aaa82a9e2e093a9047e7de1a0fa75917c
CloudS3n <402645063@qq.com>
2021-07-08 18:58:55 +0800

    feat(autoconfigure): 添加SpringContext

---
+++ uni-spring-boot-autoconfigure/src/main/java/cn/uni/starter/autoconfigure/context/SpringContext.java
@@ -0,0 +1,30 @@
+package cn.uni.starter.autoconfigure.context;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.util.Objects;
+
+/**
+ * @author CloudS3n
+ * @date 2021-07-08 18:15
+ */
+public class SpringContext implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    public static <T> T getBean(Class<T> beanClass) {
+        return context.getBean(beanClass);
+    }
+
+    public static Object getBean(String beanName) {
+        return context.getBean(beanName);
+    }
+
+    @Override
+    @SuppressWarnings("NullableProblems")
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringContext.context = Objects.requireNonNull(applicationContext);
+    }
+}
