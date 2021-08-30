46a04568d73a8f5caa18cb232cd6dac99d32cce6
CloudS3n <402645063@qq.com>
2021-08-30 13:50:30 +0800

    refactor(autoconfigure): remove jpa converter

--- uni-spring-boot-jpa-starter/src/main/java/cn/uni/starter/jpa/utils/DateConverter.java
+++ uni-spring-boot-jpa-starter/src/main/java/cn/uni/starter/jpa/utils/DateConverter.java
@@ -1,36 +1,36 @@
-package cn.uni.starter.jpa.utils;
-
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-/**
- * @author CloudS3n
- */
-@Converter(autoApply = true)
-public class DateConverter implements AttributeConverter<String, Date> {
-    @Override
-    public Date convertToDatabaseColumn(String objectValue) {
-        if (objectValue == null || "".equals(objectValue)) {
-            return null;
-        }
-
-        try {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(objectValue);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    @Override
-    public String convertToEntityAttribute(Date dateValue) {
-        if (dateValue == null) {
-            return "";
-        }
-        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(dateValue);
-    }
-}
+//package cn.uni.starter.jpa.utils;
+//
+//import javax.persistence.AttributeConverter;
+//import javax.persistence.Converter;
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+///**
+// * @author CloudS3n
+// */
+//@Converter
+//public class DateConverter implements AttributeConverter<String, Date> {
+//    @Override
+//    public Date convertToDatabaseColumn(String objectValue) {
+//        if (objectValue == null || "".equals(objectValue)) {
+//            return null;
+//        }
+//
+//        try {
+//            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(objectValue);
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+//    }
+//
+//    @Override
+//    public String convertToEntityAttribute(Date dateValue) {
+//        if (dateValue == null) {
+//            return "";
+//        }
+//        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(dateValue);
+//    }
+//}
