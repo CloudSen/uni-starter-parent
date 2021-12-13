f589b620a6c58ef8db997213c83c317ff7c2463c
clouds3n <www.402645063@gmail.com>
2021-12-13 15:36:11 +0800

    fix(common-util): common util之前没统一版本，后面再删除Response

---
+++ uni-common-util/src/main/java/cn/uni/common/util/Response.java
@@ -0,0 +1,100 @@
+package cn.uni.common.util;
+
+import lombok.Data;
+
+/**
+ * @author Awake
+ * @date 2020-07-31 09:56
+ **/
+@Data
+public class Response<T> {
+
+    public static final String SUCCESS = "success";
+    public static final String ERROR = "error";
+    public static final String VALIDATE_FAILURE = "validateFailure";
+    public static final String VALIDATE_SUCCESS = "validateSuccess";
+
+    private String status;
+
+    private String msg;
+
+    private T data;
+
+    private String code;
+
+
+    public Response() {
+
+    }
+
+    public Response(String status, String msg) {
+        this(status, msg, null);
+    }
+
+    public Response(String status, T object) {
+        this(status, null, object);
+    }
+
+    public Response(String status, String msg, T object) {
+        this.status = status;
+        this.msg = msg;
+        this.data = object;
+    }
+
+    public Response(String status, String msg, T object, String code) {
+        this.status = status;
+        this.msg = msg;
+        this.data = object;
+        this.code = code;
+    }
+
+    public static <T> Response<T> resp(String status) {
+        return resp(status, null, null);
+    }
+
+    public static <T> Response<T> resp(String status, T data) {
+        return resp(status, null, data);
+    }
+
+    public static <T> Response<T> resp(String status, String msg) {
+        return resp(status, msg, null);
+    }
+
+    public static <T> Response<T> resp(String status, String msg, T data) {
+        return resp(status, null, msg, data);
+    }
+
+    public static <T> Response<T> resp(String status, String code, String msg, T data) {
+        Response<T> respBean = new Response<>();
+        respBean.setStatus(status);
+        respBean.setMsg(msg);
+        respBean.setData(data);
+        respBean.setCode(code);
+        return respBean;
+    }
+
+
+    public static Response<?> sendValidateErrorMessage() {
+        return sendValidateErrorMessage(null);
+    }
+
+    public static <T> Response<?> sendValidateErrorMessage(T object) {
+        return sendValidateErrorMessage(VALIDATE_FAILURE, object);
+    }
+
+    public static <T> Response<?> sendValidateErrorMessage(String message, T object) {
+        return Response.resp(SUCCESS, VALIDATE_FAILURE, message, object);
+    }
+
+    public static Response<?> sendSuccessMessage() {
+        return sendSuccessMessage(null);
+    }
+
+    public static Response<?> sendSuccessMessage(Object object) {
+        return sendSuccessMessage(SUCCESS, object);
+    }
+
+    public static Response<?> sendSuccessMessage(String message, Object object) {
+        return Response.resp(SUCCESS, SUCCESS, message, object);
+    }
+}
