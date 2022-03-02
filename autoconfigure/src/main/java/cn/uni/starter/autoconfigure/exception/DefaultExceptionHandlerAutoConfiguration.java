4cb9abe5ac6d2d4362df99ecc7d1179bab1f0229
clouds3n <www.402645063@gmail.com>
2022-03-02 16:43:27 +0800

    fix(autoconfigure): 添加BindException异常处理

--- autoconfigure/src/main/java/cn/uni/starter/autoconfigure/exception/DefaultExceptionHandlerAutoConfiguration.java
+++ autoconfigure/src/main/java/cn/uni/starter/autoconfigure/exception/DefaultExceptionHandlerAutoConfiguration.java
@@ -3,14 +3,18 @@ package cn.uni.starter.autoconfigure.exception;
 import cn.uni.common.util.Response;
 import cn.uni.starter.autoconfigure.AutoConfigConstants;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -33,6 +37,16 @@ public class DefaultExceptionHandlerAutoConfiguration {
         StringBuilder msg = new StringBuilder();
         e.getBindingResult().getAllErrors().forEach(error -> msg.append(error.getDefaultMessage()).append(","));
         return Response.resp(Response.ERROR, StringUtils.isBlank(msg.toString()) ? AutoConfigConstants.SERVER_ERROR : msg.toString());
+    }
+
+    @ExceptionHandler(BindException.class)
+    public Response<?> bindExceptionHandler(BindException e) {
+        log.error(ExceptionUtils.getStackTrace(e));
+        List<ObjectError> allErrors = e.getAllErrors();
+        if (CollectionUtils.isNotEmpty(allErrors)) {
+            return Response.resp(Response.ERROR, allErrors.get(0).getDefaultMessage());
+        }
+        return Response.resp(Response.ERROR, AutoConfigConstants.ERROR_OPERATE);
     }
 
     @ExceptionHandler(UniException.class)
