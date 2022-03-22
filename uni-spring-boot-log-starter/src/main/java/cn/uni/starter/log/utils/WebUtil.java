caf6e70c60bb8f6bc32285a2f50c9591338e11d0
bailong <zxczxczx0660@163.com>
2022-03-22 18:43:11 +0800

    refactor(日志服务starter): 修复日志服务POST请求参数获取为空

--- uni-spring-boot-log-starter/src/main/java/cn/uni/starter/log/utils/WebUtil.java
+++ uni-spring-boot-log-starter/src/main/java/cn/uni/starter/log/utils/WebUtil.java
@@ -17,7 +17,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.function.Predicate;
